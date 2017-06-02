9665a1f625f18d36065f34835c21683442528ce5
aqa510415008 <510415008@qq.com>
2017-06-02 15:48:59 +0800

    修改WebMvc 项目名称

--- AL.Framework.WebApi/Properties/AssemblyInfo.cs
+++ AL.Framework.WebApi/Properties/AssemblyInfo.cs
@@ -1,23 +1,24 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("AL.API.Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AL.Framework.WebApi")]
+[assembly: AssemblyDescription("Asp.Net  WebApi WebApi基础库")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AL.API.Framework")]
+[assembly: AssemblyCompany("阿凌开源小二")]
+[assembly: AssemblyProduct("WebApi基础库")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("AL")]
 [assembly: AssemblyCulture("")]
 
 //将 ComVisible 设置为 false 将使此程序集中的类型
 //对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
 //请将此类型的 ComVisible 特性设置为 true。
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("0f294fb5-f370-4d3d-bb6b-cdf7a3b03716")]
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: NeutralResourcesLanguage("zh")]
+
